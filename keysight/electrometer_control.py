b7cbce349af4c269d3a651c7bb1e47efdbe9104f
eliasloetscher <eliasloetscher@bluewin.ch>
2020-09-21 12:54:53 +0200

    upgraded electrometer class

--- keysight/electrometer_control.py
+++ keysight/electrometer_control.py
@@ -1,5 +1,13 @@
 import pyvisa as visa
 from mviss_package.parameters import Parameters
+from pyvisa import constants
+
+
+class InterlockError(Exception):
+    """ Exception raised when an action is not possible due to open or closed interlock
+        Example: Trying to set voltage >26 V when interlock is open
+    """
+    pass
 
 
 class ElectrometerControl:
@@ -32,6 +40,49 @@ class ElectrometerControl:
 
         if self.DEBUG:
             print('*IDN? returned: %s' % idn.rstrip('\n'))
+
+    def get_voltage(self):
+        """
+        :return:
+        """
+        self.session.write('MEAS:VOLT:DC?')
+        return self.session.read()
+
+    def set_voltage(self, voltage):
+        """
+        Sets the source voltage immediately.
+        Raises ValueError if voltage is out of range (<0 V or >1000 V)
+        Raises InterlockError if voltage is > 21 V and interlock is open
+        :param voltage: Voltage to set
+        :return: None
+        """
+        if voltage < 0 or voltage > 1000:
+            raise ValueError
+
+        if int(self.get_interlock_state()) == 1 and voltage > 21 :
+            raise InterlockError
+
+        query = str(':SOUR:VOLT ' + str(voltage))
+        self.session.write(query)
+
+    def get_current(self):
+        self.session.write('MEAS:CURR:DC?')
+        return self.session.read()
+
+    def get_temperature(self):
+        """
+        :return:
+        """
+        self.session.write('SYST:TEMP?')
+        return self.session.read()
+
+    def get_interlock_state(self):
+        """
+        Reads the interlock state and returns if open or closed
+        :return: 0 (close) or 1 (open)
+        """
+        self.session.write('SYST:INT:TRIP?')
+        return self.session.read()
 
     def close_connection(self):
         # Close the connection to the instrument
--- keysight/first_test.py
+++ keysight/first_test.py
@@ -2,4 +2,10 @@ from keysight.electrometer_control import ElectrometerControl
 
 inst = ElectrometerControl()
 inst.get_idn_response()
+inst.set_voltage(21)
+print("Interlock state: ", inst.get_interlock_state())
+print("type ", type(inst.get_interlock_state()))
+print("Voltage in V:", round(float(inst.get_voltage()), 2))
+print("Current in pA:", round(float(inst.get_current())*1000*1000*1000*1000, 5))
+print("Temperature in °C:", round(float(inst.get_temperature()), 2))
 inst.close_connection()
